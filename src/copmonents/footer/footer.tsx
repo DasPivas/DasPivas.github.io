fcb5b32b0d1bdb88efc106f576fd5f90db8c1ba2
Misha Pryadun <misha.pma12@yandex.ru>
2025-11-20 18:21:39 +0500

    feat(): добавил ссылку на tg

--- src/copmonents/footer/footer.tsx
+++ src/copmonents/footer/footer.tsx
@@ -8,8 +8,19 @@ export const Footer: FC = () => {
 		<footer className={styles.footer}>
 			<Section>
 				<div className={styles.footer__inner}>
-					<div className={styles.footer__copyright}>Misha Pryadun 2025</div>
-
+					<div className={styles.footer__info}>
+						<div className={styles.footer__copyright}>Misha Pryadun 2025</div>
+						<div className={styles.footer__infoItem}>
+							<a
+								href="https://t.me/@pryadun69"
+								className={styles.footer__icon}
+								target="_blank"
+								rel="noreferrer"
+							>
+								Telegram @pryadun69
+							</a>
+						</div>
+					</div>
 					<div className={styles.footer__links}>
 						<a
 							href="https://github.com/DasPivas"
